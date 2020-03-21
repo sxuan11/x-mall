44100569dcf1404ea96c2fea8c5608e47ed49ccd
sxuan11 <sxuan97@outlook.com>
2020-03-21 19:13:34 +0800

    new

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -6,14 +6,17 @@
       @click-left="onClickLeft"
     />
       <van-field
-        v-model="username"
+        v-model="userPhone"
         name="手机号"
         label="手机号"
         placeholder="手机号"
+        :error="error"
+        :error-message="error_message"
+        :blur="checkInput(userPhone)"
         :rules="[{ required: true, message: '请填写手机号' }]"
       />
       <div style="margin: 16px;">
-        <van-button square block type="info" native-type="submit" @click="getcode">
+        <van-button ref="getButton" square block type="info" native-type="submit" @click="getcode" :disabled="disable">
           获取验证码
         </van-button>
       </div>
@@ -21,11 +24,18 @@
 </template>
 
 <script>
+  import { Toast } from 'vant'
   export default {
     data() {
       return {
-        username: '',
+        userPhone: '',
+        disable:true,
+        error:false,
+        error_message:'',
       };
+    },
+    computed:{
+      
     },
     methods: {
       onSubmit(values) {
@@ -36,6 +46,20 @@
       },
       getcode(){
         this.$router.push('/inputcode')
+      },
+      checkInput(userPhone){
+        let myreg=/^[1][3,4,5,6,7,8,9][0-9]{9}$/;
+          if (!myreg.test(userPhone)) {
+            this.error = true
+            this.disable = true;
+            this.error_message = "请输入正确的手机号"
+            return false;
+         }  else {
+            this.disable = false;
+            this.error = false;
+            this.error_message = ''
+            return true;
+         }     
       }
     }
   }
